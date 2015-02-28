4629b2b94d5a81733248b83a8dc52fa48cd36ae5
vanbrayne <lars.lindberg@xlent.se>
2015-02-28 16:12:38 +0100

    Problems with library.

---
+++ src/main/java/se/fredsfursten/hueplugin/Program.java
@@ -0,0 +1,9 @@
+package se.fredsfursten.hueplugin;
+
+import se.fredsfursten.plugintools.PhilipsHue;
+
+public class Program {
+	  public static void main(String[] args) {
+		  PhilipsHue.on();
+	  }
+}
